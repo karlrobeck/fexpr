5be8b544146715f0529ec92aa81d1e78cd4626fe
Karl robeck alferez <karlalferezfx@gmail.com>
2025-06-01 06:21:12 +0800

    Implement Display trait for Expr, ExprGroup, ExprGroupItem, and ExprGroups for improved string representation

--- src/parser.rs
+++ src/parser.rs
@@ -7,6 +7,12 @@ pub struct Expr {
     right: Token,
 }
 
+impl std::fmt::Display for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{{} {} {}}}", self.left, self.op, self.right)
+    }
+}
+
 impl Expr {
     pub fn is_zero(self) -> bool {
         self.op == SignOp::None && self.left == Token::None && self.right == Token::None
@@ -19,10 +25,25 @@ pub struct ExprGroup {
     pub item: ExprGroupItem,
 }
 
+impl std::fmt::Display for ExprGroup {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{{} {}}}", self.join, self.item)
+    }
+}
+
 #[derive(Debug)]
 pub enum ExprGroupItem {
     Expr(Expr),
     ExprGroups(ExprGroups),
+}
+
+impl std::fmt::Display for ExprGroupItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ExprGroupItem::Expr(expr) => write!(f, "{expr}",),
+            ExprGroupItem::ExprGroups(expr_groups) => write!(f, "{expr_groups}",),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -50,6 +71,20 @@ impl ExprGroups {
     }
 }
 
+impl std::fmt::Display for ExprGroups {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[")?;
+        for (i, expr_group) in self.expr_groups.iter().enumerate() {
+            if i > 0 {
+                write!(f, " ")?;
+            }
+            write!(f, "{{{} {}}}", expr_group.join, expr_group.item)?;
+        }
+        write!(f, "]")?;
+        Ok(())
+    }
+}
+
 #[derive(PartialEq)]
 enum Step {
     BeforeSign,
@@ -59,9 +94,7 @@ enum Step {
 }
 
 pub fn parse(text: &str) -> Result<ExprGroups, anyhow::Error> {
-    let mut result = ExprGroups {
-        expr_groups: vec![],
-    };
+    let mut result = ExprGroups::new();
     let mut scanner = Scanner::new(text.as_bytes().into(), 3);
     let mut step = Step::BeforeSign;
     let mut join = JoinOp::And;
@@ -199,87 +232,159 @@ mod test {
     use rstest::rstest;
 
     #[rstest]
-    #[case(r"> 1", true)]
-    #[case(r"a >", true)]
-    #[case(r"a > >", true)]
-    #[case(r"a > %", true)]
-    #[case(r"a ! 1", true)]
-    #[case(r"a - 1", true)]
-    #[case(r"a + 1", true)]
-    #[case(r"1 - 1", true)]
-    #[case(r"1 + 1", true)]
-    #[case(r"> a 1", true)]
-    #[case(r"a || 1", true)]
-    #[case(r"a && 1", true)]
-    #[case(r"test > 1 &&", true)]
-    #[case(r"|| test = 1", true)]
-    #[case(r"test = 1 && ||", true)]
-    #[case(r"test = 1 && a", true)]
-    #[case(r#"test = 1 && "a""#, true)]
-    #[case(r"test = 1 a", true)]
-    #[case(r#"test = 1 "a""#, true)]
-    #[case(r"test = 1@test", true)]
-    #[case(r"test = .@test", true)]
-    #[case(r#"test = "demo'"#, true)]
-    #[case(r#"test = 'demo""#, true)]
-    #[case(r#"test = 'demo'""#, true)]
-    #[case(r"test = 'demo''", true)]
-    #[case(r#"test = "demo"'"#, true)]
-    #[case(r#"test = "demo"""#, true)]
-    #[case(r#"test = ""demo""#, true)]
-    #[case(r"test = ''demo''", true)]
-    #[case(r"test = `demo`", true)]
-    #[case(r"test = / demo", true)]
-    #[case(r"test = // demo", true)]
-    #[case(r"// demo", true)]
-    #[case(r"test = 123 // demo", false)]
-    #[case("test = // demo\n123", false)]
+    #[case(r"> 1", true, r"[]")]
+    #[case(r"a >", true, r"[]")]
+    #[case(r"a > >", true, r"[]")]
+    #[case(r"a > %", true, r"[]")]
+    #[case(r"a ! 1", true, r"[]")]
+    #[case(r"a - 1", true, r"[]")]
+    #[case(r"a + 1", true, r"[]")]
+    #[case(r"1 - 1", true, r"[]")]
+    #[case(r"1 + 1", true, r"[]")]
+    #[case(r"> a 1", true, r"[]")]
+    #[case(r"a || 1", true, r"[]")]
+    #[case(r"a && 1", true, r"[]")]
+    #[case(r"test > 1 &&", true, r"[]")]
+    #[case(r"|| test = 1", true, r"[]")]
+    #[case(r"test = 1 && ||", true, r"[]")]
+    #[case(r"test = 1 && a", true, r"[]")]
+    #[case(r#"test = 1 && "a""#, true, r"[]")]
+    #[case(r"test = 1 a", true, r"[]")]
+    #[case(r#"test = 1 "a""#, true, r"[]")]
+    #[case(r"test = 1@test", true, r"[]")]
+    #[case(r"test = .@test", true, r"[]")]
+    #[case(r#"test = "demo'"#, true, r"[]")]
+    #[case(r#"test = 'demo""#, true, r"[]")]
+    #[case(r#"test = 'demo'""#, true, r"[]")]
+    #[case(r"test = 'demo''", true, r"[]")]
+    #[case(r#"test = "demo"'"#, true, r"[]")]
+    #[case(r#"test = "demo"""#, true, r"[]")]
+    #[case(r#"test = ""demo""#, true, r"[]")]
+    #[case(r"test = ''demo''", true, r"[]")]
+    #[case(r"test = `demo`", true, r"[]")]
+    #[case(r"test = / demo", true, r"[]")]
+    #[case(r"test = // demo", true, r"[]")]
+    #[case(r"// demo", true, r"[]")]
+    #[case(
+        r"test = 123 // demo",
+        false,
+        r"[{&& {{identifier test} = {number 123}}}]"
+    )]
+    #[case(
+        "test = // demo\n123",
+        false,
+        r"[{&& {{identifier test} = {number 123}}}]"
+    )]
     #[case(
         r"
             a = 123 &&
             // demo
             b = 456
         ",
-        false
+        false,
+        r"[{&& {{identifier a} = {number 123}}} {&& {{identifier b} = {number 456}}}]"
     )]
-    #[case(r"1=12", false)]
-    #[case(r"   1    =    12    ", false)]
-    #[case(r#""demo" != test"#, false)]
-    #[case(r"a~1", false)]
-    #[case(r"a !~ 1", false)]
-    #[case(r"test>12", false)]
-    #[case(r"test > 12", false)]
-    #[case(r#"test >="test""#, false)]
-    #[case(r"test<@demo.test2", false)]
-    #[case(r#"1<="test""#, false)]
-    #[case(r#"1<="te'st""#, false)]
-    #[case(r#"demo='te\'st'"#, false)]
-    #[case(r#"demo="te\'st""#, false)]
-    #[case(r#"demo="te\"st""#, false)]
-    #[case(r"(a=1", true)]
-    #[case(r"a=1)", true)]
-    #[case(r"((a=1)", true)]
-    #[case(r"{a=1}", true)]
-    #[case(r"[a=1]", true)]
-    #[case(r"((a=1 || a=2) && c=1))", true)]
-    #[case(r"()", true)]
-    #[case(r"(a=1)", false)]
-    #[case(r#"(a="test(")"#, false)]
-    #[case(r#"(a="test)")"#, false)]
-    #[case(r"((a=1))", false)]
-    #[case(r"a=1 || 2!=3", false)]
-    #[case(r"a=1 && 2!=3", false)]
-    #[case(r#"a=1 && 2!=3 || "b"=a"#, false)]
-    #[case(r#"(a=1 && 2!=3) || "b"=a"#, false)]
-    #[case(r"((a=1 || a=2) && (c=1))", false)]
-    #[case(r#"(a='"')"#, false)]
-    #[case(r"(a='\'')", false)]
-    #[case(r#"(a="'")"#, false)]
-    #[case(r#"(a="\"")"#, false)]
-    pub fn test_parser(#[case] text: &str, #[case] expected_error: bool) {
-        match parse(text) {
-            Ok(_) => assert!(!expected_error),
-            Err(_) => assert!(expected_error),
-        }
+    #[case(r"1=12", false, r"[{&& {{number 1} = {number 12}}}]")]
+    #[case(r"   1    =    12    ", false, r"[{&& {{number 1} = {number 12}}}]")]
+    #[case(
+        r#""demo" != test"#,
+        false,
+        r"[{&& {{text demo} != {identifier test}}}]"
+    )]
+    #[case(r"a~1", false, r"[{&& {{identifier a} ~ {number 1}}}]")]
+    #[case(r"a !~ 1", false, r"[{&& {{identifier a} !~ {number 1}}}]")]
+    #[case(r"test>12", false, r"[{&& {{identifier test} > {number 12}}}]")]
+    #[case(r"test > 12", false, r"[{&& {{identifier test} > {number 12}}}]")]
+    #[case(
+        r#"test >="test""#,
+        false,
+        r"[{&& {{identifier test} >= {text test}}}]"
+    )]
+    #[case(
+        r"test<@demo.test2",
+        false,
+        r"[{&& {{identifier test} < {identifier @demo.test2}}}]"
+    )]
+    #[case(r#"1<="test""#, false, r"[{&& {{number 1} <= {text test}}}]")]
+    #[case(r#"1<="te'st""#, false, r"[{&& {{number 1} <= {text te'st}}}]")]
+    #[case(
+        r#"demo='te\'st'"#,
+        false,
+        r"[{&& {{identifier demo} = {text te'st}}}]"
+    )]
+    #[case(
+        r#"demo="te\'st""#,
+        false,
+        r"[{&& {{identifier demo} = {text te\'st}}}]"
+    )]
+    #[case(
+        r#"demo="te\"st""#,
+        false,
+        r#"[{&& {{identifier demo} = {text te"st}}}]"#
+    )]
+    #[case(r"(a=1", true, r"[]")]
+    #[case(r"a=1)", true, r"[]")]
+    #[case(r"((a=1)", true, r"[]")]
+    #[case(r"{a=1}", true, r"[]")]
+    #[case(r"[a=1]", true, r"[]")]
+    #[case(r"((a=1 || a=2) && c=1))", true, r"[]")]
+    #[case(r"()", true, r"[]")]
+    #[case(r"(a=1)", false, r"[{&& [{&& {{identifier a} = {number 1}}}]}]")]
+    #[case(
+        r#"(a="test(")"#,
+        false,
+        r"[{&& [{&& {{identifier a} = {text test(}}}]}]"
+    )]
+    #[case(
+        r#"(a="test)")"#,
+        false,
+        r"[{&& [{&& {{identifier a} = {text test)}}}]}]"
+    )]
+    #[case(
+        r"((a=1))",
+        false,
+        r"[{&& [{&& [{&& {{identifier a} = {number 1}}}]}]}]"
+    )]
+    #[case(
+        r"a=1 || 2!=3",
+        false,
+        r"[{&& {{identifier a} = {number 1}}} {|| {{number 2} != {number 3}}}]"
+    )]
+    #[case(
+        r"a=1 && 2!=3",
+        false,
+        r"[{&& {{identifier a} = {number 1}}} {&& {{number 2} != {number 3}}}]"
+    )]
+    #[case(r#"a=1 && 2!=3 || "b"=a"#, false, r"[{&& {{identifier a} = {number 1}}} {&& {{number 2} != {number 3}}} {|| {{text b} = {identifier a}}}]")]
+    #[case(r#"(a=1 && 2!=3) || "b"=a"#, false, r"[{&& [{&& {{identifier a} = {number 1}}} {&& {{number 2} != {number 3}}}]} {|| {{text b} = {identifier a}}}]")]
+    #[case(r"((a=1 || a=2) && (c=1))", false, r"[{&& [{&& [{&& {{identifier a} = {number 1}}} {|| {{identifier a} = {number 2}}}]} {&& [{&& {{identifier c} = {number 1}}}]}]}]")]
+    #[case(r#"(a='"')"#, false, r#"[{&& [{&& {{identifier a} = {text "}}}]}]"#)]
+    #[case(r"(a='\'')", false, r"[{&& [{&& {{identifier a} = {text '}}}]}]")]
+    #[case(r#"(a="'")"#, false, r"[{&& [{&& {{identifier a} = {text '}}}]}]")]
+    #[case(r#"(a="\"")"#, false, r#"[{&& [{&& {{identifier a} = {text "}}}]}]"#)]
+    pub fn test_parser(
+        #[case] text: &str,
+        #[case] expected_error: bool,
+        #[case] expected_print: &str,
+    ) {
+        let value = match parse(text) {
+            Ok(value) => {
+                assert!(!expected_error);
+                value
+            }
+            Err(_) => {
+                assert!(expected_error);
+                return;
+            }
+        };
+
+        let token_print = value.to_string();
+
+        assert!(
+            token_print == expected_print,
+            "Expected {}, got {}",
+            expected_print,
+            token_print
+        )
     }
 }
