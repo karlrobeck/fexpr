5af9920c3b02fb8508728a613a2a5c1b917fe308
Karl robeck alferez <karlalferezfx@gmail.com>
2025-06-02 08:51:14 +0800

    Normalize error messages in FexprError enum for consistent capitalization

--- src/error.rs
+++ src/error.rs
@@ -2,31 +2,31 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum FexprError {
-    #[error("unexpected character {0}")]
+    #[error("Unexpected character {0}")]
     TokenUnexpected(String),
-    #[error("invalid number {0}")]
+    #[error("Invalid number {0}")]
     InvalidNumber(String),
-    #[error("invalid quoted text {0}")]
+    #[error("Invalid quoted text {0}")]
     InvalidQuoteText(String),
-    #[error("invalid comment {0}")]
+    #[error("Invalid comment {0}")]
     InvalidComment(String),
-    #[error("invalid identifier {0}")]
+    #[error("Invalid identifier {0}")]
     InvalidIdentifier(String),
-    #[error("invalid sign operator {0}")]
+    #[error("Invalid sign operator {0}")]
     InvalidSignOperator(String),
-    #[error("invalid join operator {0}")]
+    #[error("Invalid join operator {0}")]
     InvalidJoinOperator(String),
-    #[error("unterminated group")]
+    #[error("Unterminated group")]
     UnterminatedGroup,
-    #[error("max function depth exceeded {0}")]
+    #[error("Max function depth exceeded {0}")]
     MaxFunctionDepthExceeded(i16),
-    #[error("invalid function arguments")]
+    #[error("Invalid function arguments")]
     InvalidFunctionArguments,
-    #[error("invalid function name {0}")]
+    #[error("Invalid function name {0}")]
     InvalidFunctionName(String),
-    #[error("expected comma in function arguments {0}")]
+    #[error("Expected comma in function arguments {0}")]
     ExpectedComma(String),
-    #[error("unexpected comma in function arguments {0}")]
+    #[error("Unexpected comma in function arguments {0}")]
     UnexpectedComma(String),
     #[error("Expected left operand (identifier, text or number), got {0} ({1})")]
     ExpectedLeftOperand(String, String),
